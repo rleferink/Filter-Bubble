191f655d7aa288f588cd2fd4c3aa593b56150ac8
rolandleferink <rolandleferink@live.nl>
2019-12-14 14:59:39 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+#data = pd.read_excel (r'C:\Users\Roland\PycharmProjects\scriptie\test.xlsx', sheet_name='Sheet1')
+#df = pd.DataFrame(data, columns= ['Wat is uw leeftijd?','Op welke politieke partij heeft u bij de laatste tweede-kamer verkiezingen gestemd?'])
+#print(df)
+
+
+df = pd.read_csv (r'C:\Users\Roland\PycharmProjects\scriptie\test.csv')
+print(df)
