8c79a4b38d4dc803d58135aadd4fe8dd3fc7b0fb
Roland Leferink <rolandleferink@live.nl>
2019-12-14 19:40:37 +0100

    Werkend programma

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 import pandas as pd
+import re
 
-#data = pd.read_excel (r'C:\Users\Roland\PycharmProjects\scriptie\test.xlsx', sheet_name='Sheet1')
-#df = pd.DataFrame(data, columns= ['Wat is uw leeftijd?','Op welke politieke partij heeft u bij de laatste tweede-kamer verkiezingen gestemd?'])
+#data = pd.read_excel (r'C:\Users\Roland\PycharmProjects\scriptie\test.xlsx', sheet_name='Sheet1') #Laptop
+data = pd.read_excel (r'C:\Users\Roland Leferink\PycharmProjects\scriptie\test.xlsx', sheet_name='Sheet1') #Desktop
+df = pd.DataFrame(data, columns=["Abortus tot hoeveel weken?","Oorzaken klimaatverandering","Zwarte piet of roetveegpiet?","Gevaren vaccinaties","Gevolgen illegale immigratie"])
+df = df.rename(columns={"Abortus tot hoeveel weken?": 1, "Oorzaken klimaatverandering": 2, "Zwarte piet of roetveegpiet?": 3, "Gevaren vaccinaties": 4, "Gevolgen illegale immigratie": 5})
 #print(df)
+df.to_excel('data.xlsx')
+for i in range(len(df)):
+    for x in range(1, 6):
+        str = df.loc[i, x]
+        str = re.split("\s", str)
+        r = re.compile("http.*")
+        newlist = list(filter(r.match, str))
+        print(newlist)
+        df.at[i, x] = newlist
+df.to_excel('result.xlsx')
+
+#df.to_csv(r'C:\Users\Roland Leferink\PycharmProjects\scriptie\output.txt', header=None, index=None, mode='a', sep=' ')
+#dato = pd.read_csv('output.txt', header = None)
+#print(dato)
+#dato.to_excel('dato.xlsx', header=None)
 
 
-df = pd.read_csv (r'C:\Users\Roland\PycharmProjects\scriptie\test.csv')
-print(df)
+#data = pd.read_csv (r'C:\Users\Roland\PycharmProjects\scriptie\test.csv') #Laptop
+#data = pd.read_csv (r'C:\Users\Roland Leferink\PycharmProjects\scriptie\test.csv') #Desktop
+#df = pd.DataFrame(data, columns=['Wat is uw leeftijd?','Op welke politieke partij heeft u bij de laatste tweede-kamer verkiezingen gestemd?'])
+#print(df)
