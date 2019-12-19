fdfc289af79c5f0ad56d4f6da765323ecba19c5b
rolandleferink <rolandleferink@live.nl>
2019-12-19 15:33:30 +0100

    Splitten verbeterd

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import re
 
-#data = pd.read_excel (r'C:\Users\Roland\PycharmProjects\scriptie\test.xlsx', sheet_name='Sheet1') #Laptop
-data = pd.read_excel (r'C:\Users\Roland Leferink\PycharmProjects\scriptie\test.xlsx', sheet_name='Sheet1') #Desktop
+data = pd.read_excel (r'C:\Users\Roland\PycharmProjects\scriptie\test.xlsx', sheet_name='Sheet1') #Laptop
+#data = pd.read_excel (r'C:\Users\Roland Leferink\PycharmProjects\scriptie\test.xlsx', sheet_name='Sheet1') #Desktop
 df = pd.DataFrame(data, columns=["Abortus tot hoeveel weken?","Oorzaken klimaatverandering","Zwarte piet of roetveegpiet?","Gevaren vaccinaties","Gevolgen illegale immigratie"])
 df = df.rename(columns={"Abortus tot hoeveel weken?": 1, "Oorzaken klimaatverandering": 2, "Zwarte piet of roetveegpiet?": 3, "Gevaren vaccinaties": 4, "Gevolgen illegale immigratie": 5})
 #print(df)
@@ -10,8 +10,12 @@ df.to_excel('data.xlsx')
 for i in range(len(df)):
     for x in range(1, 6):
         str = df.loc[i, x]
+        str = re.sub(".*Web results",'', str)
+        str = re.sub(".*Webresultaten",'', str)
         str = re.split("\s", str)
-        r = re.compile("http.*")
+        #print(str)
+        #r = re.compile("http.*")
+        r = re.compile("..+\..+")
         newlist = list(filter(r.match, str))
         print(newlist)
         df.at[i, x] = newlist
